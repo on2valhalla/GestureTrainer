4f9af6f9b60a475c18f14ddd889c4d46cb4a8c28
Jason Carlisle Mann <jcm2207@columbia.edu>
2013-05-09 11:41:49 -0400

    Created simplistic user object to hold hand data

---
+++ beta/GestureTrainer/include/user.h
@@ -0,0 +1,24 @@
+/*
+
+	Created by: Jason Carlisle Mann (on2valhalla | jcm2207@columbia.edu)
+
+	Stores a users profile, containing simply two hands for now
+
+*/
+
+
+#ifndef USER_H
+#define USER_H
+
+
+#include "../include/hand.h"
+
+
+class User
+{
+
+public:
+	Hand fist;
+	Hand spread;
+	
+}
